6c9900f1876a63e54e6784b1f7e65d1cdc72e039
Swethavarnaa U <33798696+varnaa@users.noreply.github.com>
2022-07-31 09:38:06 +0530

    Time: 2 ms (50.00%), Space: 41.9 MB (37.50%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,39 +1,16 @@
 class Solution {
     public int minimumOperations(int[] nums) {
+        //base case
         if(nums.length == 0){
             return 0;
         }
         
-        int[] sortedNumbs = nums.clone();
-        Arrays.sort(sortedNumbs);
-        int result = 0;
-        
-        
-        while(allZeros(nums) != true){
-            int index = 0;
-            for(index=0; index<nums.length; index++){
-                if(sortedNumbs[index] != 0) break;
-            }
-            if(index == nums.length) return result;
-            for(int i=0; i<nums.length; i++){
-                if(nums[i] == 0) continue;
-                nums[i] = nums[i] - sortedNumbs[index]  < 0 ? 0 : nums[i] - sortedNumbs[index];
-            }
-            
-            sortedNumbs = nums.clone();
-            Arrays.sort(sortedNumbs);
-            result++;
-        }
-        
-        return result;
-    }
-    
-    private boolean allZeros(int[] nums){
+        var set = new HashSet<>();
         for(int i=0; i<nums.length; i++){
-            if(nums[i] != 0){
-                return false;
-            }
+            if(nums[i] == 0) continue;
+            set.add(nums[i]);
         }
-        return true;
+        
+        return set.size();
     }
 }
