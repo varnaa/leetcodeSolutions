dd830406389e63aea9aa5b9885a36a9f2d393b5a
Swethavarnaa U <33798696+varnaa@users.noreply.github.com>
2022-08-01 13:16:59 +0530

    Time: 1 ms (90.79%), Space: 41.3 MB (84.34%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,26 +1,9 @@
 class Solution {
     public String interpret(String command) {
-        // base case 
         if(command == null || command.length() == 0){
             return command;
         }
         
-        int length = command.length();
-        StringBuilder result = new StringBuilder("");
-        char[] commandArray = command.toCharArray();
-        
-        for(int i=0; i<commandArray.length; i++){
-            if(Character.isLetter(commandArray[i]) && commandArray[i] == 'G'){
-                result.append("G");
-            }else if(commandArray[i+1] == ')'){
-                result.append("o");
-                i++;
-            }else{
-                result.append("al");
-                i+=3;
-            }
-        }
-        
-        return result.toString();
+        return command.replace("()","o").replace("(al)","al");
     }
 }
